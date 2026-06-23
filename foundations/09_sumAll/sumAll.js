054faabfd556697c85da0da4679b8b862180d939
milanm2000 <milan.micevic6@gmail.com>
2026-06-23 19:42:57 +0200

    Solve ninth exercise, sum range of numbers

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(typeof(a) != "number" || typeof(b) != "number") return "ERROR";
+    if( a < 0 || b < 0) return "ERROR";
+    if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
 
+    let min = a < b ? a : b;
+    let max = a > b ? a : b;
+
+    let sum = 0;
+    for(let i = min; i <= max; i++){
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
