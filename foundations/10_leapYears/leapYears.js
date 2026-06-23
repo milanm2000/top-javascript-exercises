dd79adc50479847a66c3cd8edd95fe9b55590471
milanm2000 <milan.micevic6@gmail.com>
2026-06-23 19:45:33 +0200

    Solve tenth exercise, check if its leap yer

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if(! typeof(year) == "number") return "ERROR";
 
+    return year % 4 != 0 ? false : (year % 100 == 0 ? (year % 400 == 0) : true);
 };
 
 // Do not edit below this line
