ad3f88256d5834205f5c2a4ed2523d2910550833
milanm2000 <milan.micevic6@gmail.com>
2026-06-23 19:25:48 +0200

    Solve eigth exercise, removing from array

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+    for(const n of numbers){
+        let ind = arr.indexOf(n);
+        while(ind != -1){
+            arr.splice(ind, 1);
+            ind = arr.indexOf(n);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
