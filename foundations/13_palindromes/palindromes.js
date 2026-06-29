86060ff5ad8a58a031f80944bbe2c0d4de048374
milanm2000 <milan.micevic6@gmail.com>
2026-06-29 10:31:46 +0200

    Solve palindrome exercise and pass all tests

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let arr = cleanString.split('');
 
+    for(let i = 0; i < arr.length / 2; i++){
+        if(arr.at(i) != arr.at(-i -1)) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
