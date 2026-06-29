40dc4b5ea915536f09e1edc15eea37bdddd7d509
milanm2000 <milan.micevic6@gmail.com>
2026-06-29 10:38:39 +0200

    Solve fibonacci exercise and pass all tests

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if(num < 0) return "OOPS";
+    if(num == 0) return 0;
+    if(num <= 2) return 1;
+
+    arr = [1, 1];
+    for(let i = 2; i < num; i++){
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    return arr[num-1];
 };
 
 // Do not edit below this line
