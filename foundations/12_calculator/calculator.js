9c7f4d8de17a3e82dc57abb02918ee8ecd95ba52
milanm2000 <milan.micevic6@gmail.com>
2026-06-29 10:25:31 +0200

    Solve calculator exercise and pass all tests

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, currValue) => acc+=currValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, currValue) => acc *= currValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact = 1;
+  for(let i = 1; i <= a; i++) fact *= i;
+
+  return fact
 };
 
 // Do not edit below this line
