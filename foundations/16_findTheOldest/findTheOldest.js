4c6a19ecd22c168058aff7ebd2a54420607384f8
milanm2000 <milan.micevic6@gmail.com>
2026-06-29 10:58:14 +0200

    Solve findTheOldest exercise and pass tests

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let peopleWithAges = people.map(p => {
+        if("yearOfDeath" in p) p.age = p.yearOfDeath - p.yearOfBirth;
+        else p.age = (new Date().getFullYear()) - p.yearOfBirth;
+        
+        return p;
+    });
 
+    peopleWithAges.sort((p1, p2) => p2.age - p1.age);
+
+    return peopleWithAges[0];
 };
 
 // Do not edit below this line
